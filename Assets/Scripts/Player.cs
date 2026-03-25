b79161c071f5ea21cef3c5447b2513bdb5aa2e69
wassup-999 <sebasquispeperez@gmail.com>
2026-03-25 15:39:01 -0500

    Avance 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,14 @@ public class Player : MonoBehaviour
 {
     public Skill Target;
     public int Level;
-    public List<Skill> LearndSkills;
+    public int Money;    
+    public List<Skill> LearndSkills;  
     void Start()
     {
         CheckName(Target);
-        TryToLearnSkill(Target);
+        CheckCost(Target);
+        //TryToLearnSkill(Target);
+        //TryToBuySkill(Target);
     }
 
 
@@ -25,11 +28,20 @@ public class Player : MonoBehaviour
             return;
         }
         SkillManager.Instance.NameOfSkill(target);
+
+    }
+    public void CheckCost(Skill target)
+    {
+        if(target == null)
+        {
+            Debug.LogWarning("Target Cost is null");
+        }
+        SkillManager.Instance.CostOfSkill(target);
     }
     public void TryToLearnSkill(Skill target)
     {
-        if(SkillManager.Instance.TryLearnSkill(this, target, out Skill result))
-        {          
+        if (SkillManager.Instance.TryLearnSkill(this, target, out Skill result))
+        {
             if (LearndSkills.Contains(target))
             {
                 Debug.Log("Ya has aprendido esta habilidad");
@@ -40,7 +52,25 @@ public class Player : MonoBehaviour
         }
         else
         {
-            Debug.Log("Cant learn right now , requieres el nivel" + target.LevelRestriction);
+            Debug.Log("Cant learn right now , requieres el nivel : " + target.LevelRestriction);
         }
     }
+    public void TryToBuySkill(Skill target)
+    {
+        if(SkillManager.Instance.TryBuySkill(this, target, out Skill result))
+        {
+            if (LearndSkills.Contains(target))
+            {
+                Debug.Log("Ya has comprado esta habilidad");
+                return;
+            }
+            LearndSkills.Add(result);
+            Debug.Log("Habilidad añadida");
+        }
+        else
+        {
+            Debug.Log("Cant buy right now , requieres el dinero : " + target.Cost);
+        }
+    }
+
 }
--- Assets/Scripts/SkillManager.cs
+++ Assets/Scripts/SkillManager.cs
@@ -1,21 +1,32 @@
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 //trabajar las consultas del player
 public class SkillManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        if(Instance == null)
+            Instance = this;
+        else 
+            Destroy(gameObject);
+    }
     public static SkillManager Instance;
 
     [SerializeField] List<Skill> skillList;
-    
+
     public void NameOfSkill<T>(T skill) where T : Skill // funcion generica
     {
         Debug.Log(skill.SkillName);
     }
+    public void CostOfSkill<T>(T skill) where T : Skill
+    {
+        Debug.Log(skill.Cost);
+    }
 
 
-
-    //    public bool TryLearnSkill<T , TResult>(Player sender, T target, out T Result) where T : Skill //-> funcion generica / out retornar más de 1 cosa
+    //public bool VerifyCondition<T>(Player sender , T target , Func< T , bool> )
 
     public bool TryLearnSkill<T>(Player sender, T target, out T Result) where T : Skill //-> funcion generica / out retornar más de 1 cosa
     {
@@ -25,7 +36,7 @@ public class SkillManager : MonoBehaviour
             return true;
 
         }
-
+        
         else
         {
             Result = default;
@@ -33,11 +44,18 @@ public class SkillManager : MonoBehaviour
         }
             
     }
-    private void Awake()
+    public bool TryBuySkill<T>(Player sender, T target, out T Result) where T : Skill
     {
-        if(Instance == null)
-            Instance = this;
-        else 
-            Destroy(gameObject);
+        if(sender.Money >= target.Cost)
+        {
+            Result = target;
+            return true;
+        }
+        else
+        {
+            Result = default;
+            return false;
+        }
     }
+    
 }
